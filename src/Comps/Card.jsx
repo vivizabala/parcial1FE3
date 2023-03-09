e8973fbbc8d8a29ac77c178c21befeff98ba3772
vivizabala <vivinasco07@gmail.com>
2023-03-08 20:29:33 -0500

    Entrega parcial 1

--- src/Comps/Card.jsx
+++ src/Comps/Card.jsx
@@ -1,15 +1,15 @@
 
 function Card(props) {
   return (
-    <div className="card-container">
+    <div>
       {props.error === "" && 
-        <div className="message">
+        <div>
           <h2>Que bueno que te guste</h2>
           <h3>✅Tambien creo que un/a {props.animalName} es {props.animalDescriptor}</h3>
         </div>
       }
       {props.error != "" && 
-        <div className="error">
+        <div>
           <h2> 🔴Por favor chequea que la información sea correcta🔴</h2>          
         </div>
       }      
